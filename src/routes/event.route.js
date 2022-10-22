a0fea63f65d119d376708c3a2646d46ef3730051
N0Ball <n0ball@gmail.com>
2022-10-23 01:15:07 +0800

    FIX: query with invalid id will crash

--- src/routes/event.route.js
+++ src/routes/event.route.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/', eventController.getAllEvents);
 router.get('/users', eventController.getEventsByUids);
 router.get('/time', eventController.getEventByTime);
-router.get('/id/:id', eventController.getEventByid);
+router.get('/id/:id([0-9a-f]{24})', eventController.getEventByid);
 
 router.post('/', eventController.createEvent);
 
